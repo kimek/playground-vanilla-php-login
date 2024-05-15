08a952006a0e24ef73aa04e482ee4894a5274a4f
Matt Kimont <mateusz.kimont@gmail.com>
2024-05-15 09:35:43 +0100

    Add api error response on DB error - not great solution - to improve - TODO

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,13 @@ require_once 'config.php';
 try {
 	$pdo = new PDO($dsn, $dbUser, $dbPassword, $options);
 } catch (PDOException $e) {
-	echo 'We sorry, we have currently doing maintanance jobs on our side. Please try again later.';
+	$msg = 'We sorry, we have currently doing maintanance jobs on our side. Please try again later.';
+	if(!empty($_POST)) {
+		require_once 'includes/json_helper.php';
+		json_response($msg,500);
+	} else {
+		echo $msg;
+	}
 	error_log('raytrace-id ' . $e->getMessage());
 	die();
 }
