048089a6b8a883c51894ff15d05f0a0878de2aff
Matt Kimont <mateusz.kimont@gmail.com>
2024-05-15 09:15:53 +0100

    Run CSRF first on action

--- action.php
+++ action.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
+
+if(empty($_POST)) {
+	json_response(null,500);
+}
+
+if (!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+	die('CSRF token validation failed');
+}
+
 require_once 'db_connection.php';
 require_once 'includes/file_handling.php';
 require_once 'userSystem.php';
 require_once 'includes/json_helper.php';
 $userSystem = new UserSystem($pdo);
 
-if(empty($_POST)) {
-	json_response(null,500);
-}
-
 if ($_POST['action'] === 'register') {
-	if (!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
-		die('CSRF token validation failed');
-	}
-
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	$file = $_FILES['file'];
@@ -29,9 +30,6 @@ if ($_POST['action'] === 'register') {
 }
 
 if ($_POST['action'] === 'login') {
-	if (!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
-		die('CSRF token validation failed');
-	}
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	if ($userSystem->login($username, $password)) {
@@ -45,7 +43,6 @@ if ($_POST['action'] === 'logout') {
 	$_SESSION = array();
 	session_destroy();
 	json_response();
-	exit;
 }
 
 json_response('',500);
