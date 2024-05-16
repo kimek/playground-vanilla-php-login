e051af24d1bb62261680c1dffb29c2cf1d89c9e0
Matt Kimont <mateusz.kimont@gmail.com>
2024-05-16 10:00:42 +0100

    Add js and css versioning to update view

--- src/view/home-template.php
+++ src/view/home-template.php
@@ -3,7 +3,7 @@
 <head>
     <title>Test - Mateusz Kimont - Basic User Registration and Login</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/style.css?version=1.1">
 </head>
 <body>
 <?php
@@ -13,7 +13,7 @@ if (isset($_SESSION['user_id'])) {
 	include_once __DIR__ . '/user-access.php';
 }
 ?>
-<script src="assets/script.js"></script>
+<script src="assets/script.js?version=1.1"></script>
 </body>
 </html>
 
